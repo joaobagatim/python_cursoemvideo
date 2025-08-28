6ca43d1d4a3979cca2f56ffe1af8e4e7f3b0d08e
joaobagatim <joaobagatim86@gmail.com>
2025-08-28 12:34:43 -0300

    Adiciona exercicio 05 - Antecessor e Sucessor

---
+++ Exercicios/ex005.py
@@ -0,0 +1,7 @@
+# Questão 05
+# Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor
+
+num = int(input("Digite um número: "))
+antecessor = num - 1
+sucessor = num + 1
+print(f"Analisando o valor {num}, seu antecessor é {antecessor} e o sucessor é {sucessor}")
